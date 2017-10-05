b2006a1b0fd62055e239752d4184fa15c2934a1e
lesopi <karine.perrard@epfl.ch>
2017-10-05 14:26:50 +0200

    template

---
+++ implementations.py
@@ -0,0 +1,20 @@
+
+
+
+def least squares GD(y, tx, initial w, max iters, gamma):
+    #Linear regression using gradient descent
+
+def least squares SGD(y, tx, initial w, max iters, gamma):
+    #Linear regression using stochastic gradient descent
+
+def least squares(y, tx):
+    #Least squares regression using normal equations
+
+def ridge regression(y, tx, lambda ):
+    #Ridge regression using normal equations
+
+def logistic regression(y, tx, initial w, max iters, gamma):
+    #Logistic regression using gradient descent or SGD
+
+def reg logistic regression(y, tx, lambda , initial w, max iters, gamma):
+    #Regularized logistic regression using gradient descent or SGD
