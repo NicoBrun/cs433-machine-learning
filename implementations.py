40fddd8670a2910baa93c9aa549c0176754d8bf9
Nicolas Brunner <nicolas.brunner@epfl.ch>
2017-10-05 14:54:55 +0200

    Added underscore

--- implementations.py
+++ implementations.py
@@ -1,20 +1,25 @@
+pass
 
-
-
-def least_squares_GD(y, tx, initial w, max iters, gamma):
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    pass
     #Linear regression using gradient descent
 
-def least_squares_SGD(y, tx, initial w, max iters, gamma):
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    pass
     #Linear regression using stochastic gradient descent
 
 def least_squares(y, tx):
+    pass
     #Least squares regression using normal equations
 
-def ridge_regression(y, tx, lambda ):
+def ridge_regression(y, tx, lambda_ ):
+    pass
     #Ridge regression using normal equations
 
-def logistic_regression(y, tx, initial w, max iters, gamma):
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
     #Logistic regression using gradient descent or SGD
 
-def reg_logistic_regression(y, tx, lambda , initial w, max iters, gamma):
+def reg_logistic_regression(y, tx, lambda_ , initial_w, max_iters, gamma):
+    pass
     #Regularized logistic regression using gradient descent or SGD
