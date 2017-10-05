4c985f8c32d6bec76313eaafbc96720debfa0cab
lesopi <karine.perrard@epfl.ch>
2017-10-05 14:46:47 +0200

    function renaming

--- implementations.py
+++ implementations.py
@@ -1,20 +1,20 @@
 
 
 
-def least squares GD(y, tx, initial w, max iters, gamma):
+def least_squares_GD(y, tx, initial w, max iters, gamma):
     #Linear regression using gradient descent
 
-def least squares SGD(y, tx, initial w, max iters, gamma):
+def least_squares_SGD(y, tx, initial w, max iters, gamma):
     #Linear regression using stochastic gradient descent
 
-def least squares(y, tx):
+def least_squares(y, tx):
     #Least squares regression using normal equations
 
-def ridge regression(y, tx, lambda ):
+def ridge_regression(y, tx, lambda ):
     #Ridge regression using normal equations
 
-def logistic regression(y, tx, initial w, max iters, gamma):
+def logistic_regression(y, tx, initial w, max iters, gamma):
     #Logistic regression using gradient descent or SGD
 
-def reg logistic regression(y, tx, lambda , initial w, max iters, gamma):
+def reg_logistic_regression(y, tx, lambda , initial w, max iters, gamma):
     #Regularized logistic regression using gradient descent or SGD
