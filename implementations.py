e5eb3b2d72575170cb52f0d83429c3607539d120
unknown <lea.bommottet@epfl.ch>
2017-10-12 14:58:43 +0200

    least squares and ridge

--- implementations.py
+++ implementations.py
@@ -1,4 +1,12 @@
-pass
+import numpy as np
+
+## pour le moment helper pour faire fonctionner les autres
+def MSE(y,tx,w) :
+    e = y-np.dot(tx,w)
+    e = np.square(e)
+    mse = np.sum(e)
+    mse = mse/len(y)
+    return mse
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     pass
@@ -9,12 +17,28 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     #Linear regression using stochastic gradient descent
 
 def least_squares(y, tx):
-    pass
     #Least squares regression using normal equations
+    xtx = np.dot(np.transpose(tx), tx)
+
+    rank = np.linalg.matrix_rank(xtx)
+
+    if (rank < len(xtx[0])):
+        # non invertible matrix
+        w = np.dot(np.linalg.pinv(tx), y)
+    else:
+        # invertible matrix
+        w = np.dot(np.linalg.inv(xtx), np.dot(np.transpose(tx), y))
+
+    return MSE(y, tx, w), w
+
 
 def ridge_regression(y, tx, lambda_ ):
-    pass
-    #Ridge regression using normal equations
+    # Ridge regression using normal equations
+    xtxli = np.dot(np.transpose(tx), tx) + (lambda_ * 2 * len(y)) * np.identity(len(tx[0]))
+    w = np.dot(np.linalg.inv(xtxli), np.dot(np.transpose(tx), y))
+    mse = MSE(y, tx, w)
+    return mse, w
+
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     pass
